e5ead834a8f94d982b6049a9b97551a00f5de331
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-12-25 10:41:19 -0600

    updated image files

--- AWS_movie_api/index.js
+++ AWS_movie_api/index.js
@@ -12,11 +12,11 @@ const app = express();
 const morgan = require("morgan");
 
 const AWS = require('aws-sdk');
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
 
 const s3Client = new S3Client({
   region: "us-east-1",
