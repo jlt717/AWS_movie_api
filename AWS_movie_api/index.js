bc830c24083dbdd7a7a2952859473e824b8502e5
jlt717 <128320420+jlt717@users.noreply.github.com>
2024-01-30 13:48:11 -0600

    update upload logic

--- AWS_movie_api/index.js
+++ AWS_movie_api/index.js
@@ -73,7 +73,7 @@ app.post("/upload/:username", async (req, res) => {
     const params = {
       Bucket: "my-bucket-for-uploading-retrieving-listing-objects",
       Key: `original-images/${image.name}`,
-      Body: image.data,
+      Body: fs.createReadStream(localImagePath),
     };
 
     try {
